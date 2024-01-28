45028019d1e982ae78050f1f1c21bbae903fc5f9
mkskh <maksym.kharchenko@dci-student.org>
2024-01-28 16:58:11 +0100

    added  info bar

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ root.title('Weather App')
 root.geometry('900x500+300+200')
 root.resizable(False,False)
 
+
+def get_weather():
+    pass
+
 #searchbox
 search_img = PhotoImage(file='img/search.png')
 insert_search_img = Label(image=search_img)
@@ -25,7 +29,8 @@ text_box.focus()
 
 search_icon = PhotoImage(file='img/search_icon.png')
 insert_search_icon = Button(image=search_icon, borderwidth=0, bg='black', cursor='hand2',  
-                            activebackground='black', highlightthickness=0, highlightcolor="black", takefocus=True)
+                            activebackground='black', highlightthickness=0, highlightcolor="black", 
+                            takefocus=True, command=get_weather)
 insert_search_icon.place(x=400, y=34)
 
 #logo
@@ -36,6 +41,38 @@ insert_logo_img.place(x=150, y=100)
 
 #labelbar
 
+bar_img = PhotoImage(file='img/box.png')
+insert_box_img = Label(image=bar_img)
+insert_box_img.pack(padx=5, pady=5, side=BOTTOM)
+
 #labels
+
+label1 = Label(root, text='WIND', font=('Heövetica', 15, 'bold'), fg='white', bg='#1ab5ef')
+label1.place(x=120, y=400)
+
+label2 = Label(root, text='HUMIDITY', font=('Heövetica', 15, 'bold'), fg='white', bg='#1ab5ef')
+label2.place(x=250, y=400)
+
+label3 = Label(root, text='DESCRIPTION', font=('Heövetica', 15, 'bold'), fg='white', bg='#1ab5ef')
+label3.place(x=438, y=400)
+
+label4 = Label(root, text='PRESSURE', font=('Heövetica', 15, 'bold'), fg='white', bg='#1ab5ef')
+label4.place(x=650, y=400)
+
+
+t = Label(font=('arial', 20, 'bold'), fg='black')
+t.place(x=400, y=150)
+c = Label(font=('arial', 20, 'bold'))
+c.place(x=400, y=250)
+
+w = Label(text='...', font=('arial', 20, 'bold'), bg='#1ab5ef')
+w.place(x=120, y=430)
+w = Label(text='...', font=('arial', 20, 'bold'), bg='#1ab5ef')
+w.place(x=280, y=430)
+w = Label(text='...', font=('arial', 20, 'bold'), bg='#1ab5ef')
+w.place(x=450, y=430)
+w = Label(text='...', font=('arial', 20, 'bold'), bg='#1ab5ef')
+w.place(x=670, y=430)
+
 
 root.mainloop()
